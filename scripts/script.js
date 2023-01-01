7a511c8fe62bed837348f2c1edcefa416bf606af
jrmartinsg <juniors2martins@hotmail.com>
2022-12-31 22:53:48 -0300

    edit botao

--- scripts/script.js
+++ scripts/script.js
@@ -11,22 +11,6 @@ let carne = document.getElementById("carne")
 let cerveja = document.getElementById("cerveja")
 let bebidas = document.getElementById("bebidas")
 
-function calcular() {
-
-    let adultos = inputAdultos.value
-    let criancas = inputCriancas.value
-    let duracao = inputDuracao.value
-
-    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)) / 2 * criancas
-    let qdtTotalCerveja = cervejaPP(duracao) * adultos
-    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao)) / 2 * criancas
-
-    carne.innerHTML = `<img src='./img/carne.png' <p>${qdtTotalCarne / 1000} Kg de Carne`
-    cerveja.innerHTML = `<img src='./img/cerveja.png' <p>${Math.ceil(qdtTotalCerveja / 355)} Latas de Cerveja`
-    bebidas.innerHTML = `<img src='./img/garrafa.png' <p>${Math.ceil(qdtTotalBebidas / 2000)} Garrafas de Bebidas`
-
-}
-
 function carnePP(duracao) {
     if (duracao >= 6) {
         return 650
@@ -54,5 +38,21 @@ function bebidasPP(duracao) {
     else {
         return 1000
     }
+
+}
+
+function calcular() {
+
+    let adultos = inputAdultos.value
+    let criancas = inputCriancas.value
+    let duracao = inputDuracao.value
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)) / 2 * criancas
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao)) / 2 * criancas
+
+    carne.innerHTML = `<img src='./img/carne.png' <p>  ${qdtTotalCarne / 1000} Kg de Carne`
+    cerveja.innerHTML = `<img src='./img/cerveja.png' <p>  ${Math.ceil(qdtTotalCerveja / 355)} Latas de Cerveja`
+    bebidas.innerHTML = `<img src='./img/garrafa.png' <p>  ${Math.ceil(qdtTotalBebidas / 2000)} Garrafas de Bebidas`
 
 }
