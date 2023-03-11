382f4b17ad35545f2b0b20fab61e8100bcdd5ce8
jrmartinsg <juniors2martins@hotmail.com>
2023-03-11 13:54:05 -0300

    edit if duracao

--- scripts/script.js
+++ scripts/script.js
@@ -17,12 +17,7 @@ function calcular() {
   let cerveja = 1200;
   let refriAgua = 1000;
 
-  if (duracao >= 6) {
-    carne = 650;
-    cerveja = 2000;
-    refriAgua = 1500;
-  } 
-  else if (duracao == 0 && adultos == 0 && criancas == 0) {
+  if (duracao == 0 && adultos == 0 && criancas == 0) {
     alert("Preencha todos os campos!");
   }
   else if (duracao > 0 && adultos == 0 && criancas == 0) {
@@ -57,5 +52,39 @@ function calcular() {
     resultado.innerHTML = `<img src='./img/carne.png' width='32px'<p>   ${Math.ceil(carneSoma / 1000)} Kg de Carne!</p>`;
     resultado.innerHTML += `<img src='./img/cerveja.png' width='32px'<p>   ${Math.ceil(cervejaSoma / 355)} Latas de Cerveja!</p>`;
     resultado.innerHTML += `<img src='./img/garrafa.png' width='32px'<p>   ${Math.ceil(refriSoma / 2000)} Garrafas de Bebidas!</p>`;
+  }
+  
+  if (duracao >= 6) {
+    carne = 650;
+    cerveja = 2000;
+    refriAgua = 1500;
+
+    if (adultos == 0) {
+      let carneSoma = (carne * criancas) / 2;
+      let cervejaSoma = 0;
+      let refriSoma = (refriAgua * criancas) / 2;
+  
+      resultado.innerHTML = `<img src='./img/carne.png' width='32px'<p>   ${Math.ceil(carneSoma / 1000)} Kg de Carne!</p>`;
+      resultado.innerHTML += `<img src='./img/cerveja.png' width='32px'<p>   ${Math.ceil(cervejaSoma)} Latas de Cerveja!</p>`;
+      resultado.innerHTML += `<img src='./img/garrafa.png' width='32px'<p>   ${Math.ceil(refriSoma / 2000)} Garrafas de Bebidas!</p>`;
+    }
+    else if (criancas == 0) {
+      let carneSoma = carne * adultos;
+      let cervejaSoma = cerveja * adultos;
+      let refriSoma = refriAgua * adultos;
+  
+      resultado.innerHTML = `<img src='./img/carne.png' width='32px'<p>   ${Math.ceil(carneSoma / 1000)} Kg de Carne!</p>`;
+      resultado.innerHTML += `<img src='./img/cerveja.png' width='32px'<p>   ${Math.ceil(cervejaSoma / 355)} Latas de Cerveja!</p>`;
+      resultado.innerHTML += `<img src='./img/garrafa.png' width='32px'<p>   ${Math.ceil(refriSoma / 2000)} Garrafas de Bebidas!</p>`;
+    }
+    else {
+      let carneSoma = carne * adultos + (carne * criancas) / 2;
+      let cervejaSoma = cerveja * adultos;
+      let refriSoma = refriAgua * adultos + (refriAgua * criancas) / 2;
+  
+      resultado.innerHTML = `<img src='./img/carne.png' width='32px'<p>   ${Math.ceil(carneSoma / 1000)} Kg de Carne!</p>`;
+      resultado.innerHTML += `<img src='./img/cerveja.png' width='32px'<p>   ${Math.ceil(cervejaSoma / 355)} Latas de Cerveja!</p>`;
+      resultado.innerHTML += `<img src='./img/garrafa.png' width='32px'<p>   ${Math.ceil(refriSoma / 2000)} Garrafas de Bebidas!</p>`;
+    }
   }
 }
